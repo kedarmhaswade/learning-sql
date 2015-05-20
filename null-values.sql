c4f791fe3ad002578b7299b63134454cf2db0a5c
Kedar <kedar@jymob.com>
2015-05-20 07:34:45 -0700

    example illustrating the null values

---
+++ null-values.sql
@@ -0,0 +1,18 @@
+/*
+  From the paper: http://www09.sigmod.org/sigmod/record/issues/0809/p23.grant.pdf
+  Flaw in SQL pointed out by CJ Date.
+*/
+drop database if exists null_values;
+create database null_values;
+use null_values;
+
+create table suppliers(sno varchar(30), city varchar(20));
+create table parts(pno varchar(30), city varchar(20));
+
+insert into suppliers (sno, city) values ('S1', 'London');
+insert into parts (pno) values ('P1');
+
+select sno, pno
+From suppliers, parts
+Where suppliers.city <> parts.city
+Or parts.city <> ’Paris’;
